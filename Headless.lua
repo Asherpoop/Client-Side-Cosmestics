2809ac2a9410b3f74be209d23c5a4b5c0829f5cf
Asherpoop <107870073+Asherpoop@users.noreply.github.com>
2022-10-21 20:45:41 +0500

    Update Headless.lua

--- Headless.lua
+++ Headless.lua
@@ -4,4 +4,10 @@ for i,v in pairs(game.Players.LocalPlayer.Character.Head:GetChildren()) do
 if (v:IsA("Decal")) then
 v.Transparency = 1
 end
+  OrionLib:MakeNotification({
+	Name = "Executed",
+	Content = "Headless Executed
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
 end
